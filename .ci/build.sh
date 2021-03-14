c3ba706bda222bdd50a02815fccb404e3609f856
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-03-14 18:05:41 +0100

    Add missing github-releases-api.bash

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,7 +3,7 @@
 # Exit this script immediately if a command/function exits with a non-zero status.
 set -e
 
-SCRIPT_INCLUDES="log.bash utils.bash setup-secrets.bash openjdk.bash maven.bash sourceforge-api.bash"
+SCRIPT_INCLUDES="log.bash utils.bash setup-secrets.bash openjdk.bash maven.bash sourceforge-api.bash github-releases-api.bash"
 # shellcheck source=inc/fetch_ci_scripts.bash
 source "$(dirname "$0")/inc/fetch_ci_scripts.bash" && fetch_ci_scripts
 
